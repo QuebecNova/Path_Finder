0e36d7b8e50e6fec063b10b277c6be1be458f06b
QuebecNova <selllmenya@gmail.com>
2022-08-10 15:09:25 +0300

    added description

---
+++ components/Description.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../styles/description.module.scss'
+
+export default function Description() {
+  return (
+    <div className={styles.description}>
+        <h2>
+        <p>Choose pathfinding algorithm to animate path from green dot to red dot.</p>
+        <p>You can draw walls by mouse.</p>
+        <p>Finally, click `Animate!` to start animation.</p>
+        </h2>
+    </div>
+  )
+}
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,9 +1,11 @@
 import styles from '../styles/Main.module.scss'
+import Description from './Description'
 import PathFinder from './PathFinder'
 
 export default function Main() {
   return (
     <div className={styles.main}>
+        <Description/>
         <PathFinder/>
     </div>
   )
